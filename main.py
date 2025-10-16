0e7b4ebd507d8bcb3766d530d2071171074ea3ea
TaalayDzh <taalay.dzhumashev@yandex.ru>
2025-10-16 15:00:31 +0600

    main

--- main.py
+++ main.py
@@ -5,9 +5,27 @@ def main(page: ft.Page):
     page.title = 'Мое первое приложение!'
     page.theme_mode = ft.ThemeMode.LIGHT
 
-
     greeting_text = ft.Text('Hello World')
+    name_input = ft.TextField(label='Введите имя:')
 
-    page.add(greeting_text)
+    def on_button_click(_):
+        name = name_input.value.strip()
+        print(name)
+
+
+        if name:
+            greeting_text.value = f'Hello{name}'
+            name_input.value = ''
+        else:
+            print('Ничего не введено')
+            greeting_text.value = 'Пожалуйста введите имя'
+
+        page.update()
+
+    name_button = ft.ElevatedButton('SEND',icon=ft.Icons.SEND, on_click=on_button_click)
+    # name_button_text = ft.TextButton('SEND')
+    # name_button_icon = ft.TextButton(icon=ft.Icons.SEND)
+    
+    page.add(greeting_text,name_input, name_button)
 
 ft.app(target=main)
